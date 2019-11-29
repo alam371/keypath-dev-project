a88cd58eb10b2e4d01e8dbf8597e2d740631e3b5
Angeline Lam <angelinelam@Angelines-MacBook-Pro.local>
2019-11-28 22:29:12 -0500

    add more js

--- scripts/index.js
+++ scripts/index.js
@@ -5,71 +5,17 @@ const chart = new Chart(ctx, {
     type: 'line',
     // The data for our dataset
     data: {
-        labels: ['$1', '$2', '$3', '$4', '$5', '$6', '$7', '$8', '$9', '$10'],
+        labels: ['$0', '$10', '$20', '$30', '$40', '$50', '$60', '$70', '$80', '$90'],
         datasets: [{
             label: 'The number of knitted hat sold',
             backgroundColor: 'rgb(251, 159, 164)',
             borderColor: 'rgb(40,48,65)',
-            data: [0, 800, 700, 6000, 5000, 4000, 3000, 2000, 1000, 0]
+            data: [0, 118, 135, 167, 180, 160, 140, 117, 98, 0]
         }]
     },
     options: {
     }
 });
-
-
-
-//Exercise Section:
-// Equilibrium Price Equations:
-//  Qsupply = Msupply * price + Bsupply
-//  Qdemand = Mdemand * price + Bdemand
-
-const Mdemand = -1000;
-const Bdemand = 10000;
-const Msupply = 0; // what if ABC can hire more people when price goes up?
-const Bsupply = 8000;
-let consumption;
-let supply;
-let message;
-
-document.getElementById("calculate").addEventListener("click", calculateOutput);
-
-function calculateOutput() {
-    let price;
-    let priceOptions = document.getElementsByName("price");
-
-    message = "";
-
-    for (let i = 1; i < priceOptions.length; i++) {
-        if (priceOptions[i].checked) {
-            price = priceOptions[i].value;
-            break;
-        }
-    }
-
-    consumption = price * Mdemand + Bdemand;
-    supply = price * Msupply + Bsupply;
-
-    if (consumption > supply) {
-        consumption = supply;
-        message = "ABC Company canot make enough XYZ Widgets";
-    }
-
-    if (consumption <= 0) {
-        consumption = 0;
-        message = "No one will buy XYZ Widgets at this price";
-    }
-
-    /*
-    if (maxRevenue) {
-        message = "This is the equilibrium price"
-    }
-    */
-
-    revenue = consumption * price;
-
-    document.getElementById("result").innerHTML = "XYZ Widgets sold:"+consumption+"/month<br>Revenue:"+revenue+"/month<br><br>"+message;
-}
 
 //giphy-API
 let apiKey = 'Yk9AWq3iW0442FfQxysOai4O7mMbB5QZ'
@@ -78,22 +24,59 @@ document.addEventListener('DOMContentLoaded', init);
 function init(){
     document.getElementById('calculate').addEventListener('click', evt => {
         evt.preventDefault();
-        let url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apiKey}&limit=3`
-        console.log(url);
-        fetch(url)
-            .then(res => res.json())
-            .then( content => {
-                console.log(content)
-                console.log('META', content.meta)
-                let img = document.createElement('img');
-                img.src = content.data[0].images.downsized.url;
-                img.alt = content.data[0].title;
-                $('#result').append(img);
-                let oneGiphy = document.querySelector('.giphy-container');
-                //oneGiphy.insertAdjacentElement('afterbegin', fig);
-            })
-            .catch(err => {
-                console.error(err);
-            })
+        // let url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apiKey}&limit=3`
+        // console.log(url);
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then( content => {
+        //         console.log(content)
+        //         console.log('META', content.meta)
+        //         let img = document.createElement('img');
+        //         img.src = content.data[0].images.downsized.url;
+        //         img.alt = content.data[0].title;
+        //         $('#result').append(img);
+        //         let oneGiphy = document.querySelector('.giphy-container');
+        //         //oneGiphy.insertAdjacentElement('afterbegin', fig);
+        //     })
+        //     .catch(err => {
+        //         console.error(err);
+        //     });
+
+            let radioOne = document.getElementById('value-one').checked;
+            let radioTwo = document.getElementById('value-two').checked;
+            let radioThree = document.getElementById('value-three').checked;
+            let question = document.forms['exercise']['price'].value;
+            let ans = '2';
+            if (radioOne === false && radioTwo === false && radioThree === false) {
+                $('.form-submit-btn').append(`<p class="alert-submit">Please choose an option before submit.</p>`);
+                $('.alert-submit').fadeOut(3000);
+                return false
+            } else {
+                if (question !== ans ){
+                    $('.form-submit-btn').append(`<p class="try-again-msg">Wrong answer. Please try again</p>`);
+                    $('.try-again-msg').fadeOut(3000);
+                    return false
+                } else {
+                    $('.giphy-container').append(`<p class="correct-msg">Correct, you got it!</p>`)
+                    let url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apiKey}&limit=3`
+                    console.log(url);
+                    fetch(url)
+                        .then(res => res.json())
+                        .then( content => {
+                            console.log(content)
+                            console.log('META', content.meta)
+                            let img = document.createElement('img');
+                            img.src = content.data[0].images.downsized.url;
+                            img.alt = content.data[0].title;
+                            $('#result').append(img);
+                            let oneGiphy = document.querySelector('.giphy-container');
+                            //oneGiphy.insertAdjacentElement('afterbegin', fig);
+                        })
+                        .catch(err => {
+                            console.error(err);
+                        });
+                }
+                return true
+            }
     })
 }
